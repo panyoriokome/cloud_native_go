2886d7f1fb423cba9d84c06be95246a07eed0d1d
yuuwatanabe <panyoriokome@gmail.com>
2021-05-09 19:53:08 +0900

    PUTのエンドポイントを追加

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module cloud_native_go
 
 go 1.15
+
+require (
+	github.com/go-errors/errors v1.2.0
+	github.com/gorilla/mux v1.8.0
+)
--- main.go
+++ main.go
@@ -1,9 +1,55 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func helloGoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello net/http!\n"))
+}
+
+func helloMuxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello gorilla/mux!\n"))
+}
+
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	// Request Bodyからキーの値を取得する
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	// キーの値の取得に失敗したらInternal Server Errorを返す
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	// Putに失敗したらInternal Server Errorを返す
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
 
 func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
 
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 var store = make(map[string]string)
