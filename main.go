dd98c42ea800f954fd4d15bcfcd00adc0fc37c31
yuuwatanabe <panyoriokome@gmail.com>
2021-05-14 19:52:53 +0900

    concurrentに対応

--- main.go
+++ main.go
@@ -5,17 +5,18 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/mux"
 )
 
-func helloGoHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello net/http!\n"))
-}
+// var store = make(map[string]string)
+var store = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
 
-func helloMuxHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello gorilla/mux!\n"))
-}
+var ErrorNoSuchKey = errors.New("no such key")
 
 func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -45,25 +46,63 @@ func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
 
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := Get(key)
+	// キーが見つからなければNot Foundを返す
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value))
+}
+
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	err := Delete(key)
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Delete Compeleted"))
+
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-var store = make(map[string]string)
-
 func Put(key string, value string) error {
-	store[key] = value
+	// store[key] = value
+	store.Lock()
+	store.m[key] = value
+	store.Unlock()
 
 	return nil
 }
 
-var ErrorNoSuchKey = errors.New("no such key")
-
 func Get(key string) (string, error) {
-	value, ok := store[key]
+	// value, ok := store[key]
+	store.Lock()
+	value, ok := store.m[key]
+	store.Unlock()
 
 	if !ok {
 		return "", ErrorNoSuchKey
@@ -73,6 +112,7 @@ func Get(key string) (string, error) {
 }
 
 func Delete(key string) error {
+	store.Lock()
 	delete(store, key)
 
 	return nil
